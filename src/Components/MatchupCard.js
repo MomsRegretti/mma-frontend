aeb5e82c957a0f393cd3f286f925f7668c79b33e
CharlieRojas <crc_185@yahoo.com>
2022-05-12 14:06:22 -0500

    full functionality

--- src/Components/MatchupCard.js
+++ src/Components/MatchupCard.js
@@ -10,7 +10,26 @@ const handleDelete = () => {
     .then(r=>r.json())
     .then(data=>handlesetMatches(data)))
 }
-
+const handleUpdate = () =>{
+  const newfighter1 = match.fighter2
+  const newfighter1_img = match.fighter2_img
+  const newfighter2 = match.fighter1
+  const newfighter2_img = match.fighter1_img
+  fetch(`http://localhost:9292/savedmatchups/${match.id}`,{
+    method: 'PATCH',
+    body: JSON.stringify({
+      fighter1: newfighter1,
+      fighter1_img : newfighter1_img,
+      fighter2 : newfighter2,
+      fighter2_img : newfighter2_img
+    }),
+    headers: {
+      'Content-type': "application/json"
+    }
+  })
+  .then(r=> r.json())
+  .then(data=>handlesetMatches(data))
+}
   return (
     <div>
       <div className='matchupCard'>
@@ -19,6 +38,7 @@ const handleDelete = () => {
         <h2>{match.fighter1} vs {match.fighter2}</h2>
       </div>
       <button className='button' onClick={handleDelete}>Delete Matchup</button>
+      <button className='button1' onClick={handleUpdate}>NEW CHALLENGER</button>
     </div>
   )
 }
